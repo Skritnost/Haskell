83d688566086c859902ff3c3c714b3444e6a4f57
Andrey Komarov <32583963+Skritnost@users.noreply.github.com>
2017-10-07 02:24:43 +0300

    Add files via upload

--- lab1.hs
+++ lab1.hs
@@ -1,6 +1,5 @@
 data Term = IntConstant{ intValue :: Int }    
             | Variable{ varName :: String }    
-            | BinaryTerm{ lhv :: Term, rhv :: Term }
 	    | Sum1 { lSum1 :: Term, rSum1 :: Term}
 	    | Product1 { lProduct1 :: Term, rProduct1 :: Term}
 	    | Sub1 { lSub1 :: Term, rSub1 :: Term }
@@ -13,4 +12,10 @@ value1 <-> value2 = Sub1 value1 value2
 value1 <*> value2 = Product1 value1 value2
 
 replaceVar :: String -> Term -> Term
-
+replaceVar var Variable { varName = var1}
+		| var1 == var = "the names are equal, enter a different name"
+		| otherwise = Variable var1
+replaceVar var Sum1 { lSum1, rSum1 } = Sum1 (replaceVar var lSum1) (replaceVar var rSum1)
+replaceVar var Product1 { lProduct1, rProduct1 } = Product1 (replaceVar var lProduct1) (replaceVar var rProduct1)
+replaceVar var Sub1 { lSub1, rSub1 } = Sub1 (replaceVar var lSub1) (replaceVar var lSub1)
+replaceVar var UnMin { unMin } = UnMin (replaceVar var unMin) 
