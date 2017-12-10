c1204cb356a73153208a28e0d909d6a215536adc
Andrey Komarov <32583963+Skritnost@users.noreply.github.com>
2017-12-10 22:01:05 +0300

    Update lab1.hs

--- lab1.hs
+++ lab1.hs
@@ -1,21 +1,25 @@
 data Term = IntConstant{ intValue :: Int }    
             | Variable{ varName :: String }    
-	    | Sum1 { lSum1 :: Term, rSum1 :: Term}
-	    | Product1 { lProduct1 :: Term, rProduct1 :: Term}
-	    | Sub1 { lSub1 :: Term, rSub1 :: Term }
-            | UnMin { unMin :: Term}
-	    deriving(Show,Eq)
-
-<+>, <->, <*> :: Term -> Term -> Term
-value1 <+> value2 = Sum1 value1 value2
-value1 <-> value2 = Sub1 value1 value2
-value1 <*> value2 = Product1 value1 value2
-
-replaceVar :: String -> Term -> Term
-replaceVar var Variable { varName = var1}
-		| var1 == var = "the names are equal, enter a different name"
-		| otherwise = Variable var1
-replaceVar var Sum1 { lSum1, rSum1 } = Sum1 (replaceVar var lSum1) (replaceVar var rSum1)
-replaceVar var Product1 { lProduct1, rProduct1 } = Product1 (replaceVar var lProduct1) (replaceVar var rProduct1)
-replaceVar var Sub1 { lSub1, rSub1 } = Sub1 (replaceVar var lSub1) (replaceVar var lSub1)
-replaceVar var UnMin { unMin } = UnMin (replaceVar var unMin) 
+            | Add{ lAdd :: Term, rAdd :: Term } 
+            | Sub{ lSub :: Term, rSub :: Term } 
+            | Mul{ lMul :: Term, rMul :: Term } 
+            | Neg{ neg :: Term } 
+            deriving(Show,Eq)
+ 
+(<+>),(<->),(<*>) :: Term -> Term -> Term
+ 
+l <+> r = Add l r
+l <-> r = Sub l r
+l <*> r = Mul l r
+ 
+infixl 6 <+>,<->
+infixl 7 <*>
+ 
+replaceVar :: String -> Int -> Term -> Term
+replaceVar _ _ t@IntConstant{} = t 
+replaceVar s v Variable{ varName = s' } | s==s' = IntConstant v
+                                        | otherwise = Variable s'
+replaceVar s v Add{lAdd=l,rAdd=r} = Add (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Sub{lSub=l,rSub=r} = Sub (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Mul{lMul=l,rMul=r} = Mul (replaceVar s v l) (replaceVar s v r)
+replaceVar s v Neg{ neg=n} = Neg (replaceVar s v n)
